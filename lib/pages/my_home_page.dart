207658692cc9191b17f2be14b9dacc47e3a0bffa
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2020-08-21 07:23:44 +0630

    1. UI is updated. 2. Remove unused packages.

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -4,7 +4,35 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Flutter GetX')),
+      body: body,
     );
   }
+
+  Widget get body => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          countDisplay,
+          increaseButton,
+          decreaseButton,
+        ],
+      );
+
+  Widget get countDisplay => Align(
+        alignment: Alignment.center,
+        child: Text('Count: '), // TODO: display count
+      );
+
+  Widget get increaseButton => RaisedButton(
+        onPressed: () {
+          // TODO: increase count
+        },
+        child: Text('Increase'),
+      );
+
+  Widget get decreaseButton => RaisedButton(
+        onPressed: () {
+          // TODO: decrease count
+        },
+        child: Text('Decrease'),
+      );
 }
